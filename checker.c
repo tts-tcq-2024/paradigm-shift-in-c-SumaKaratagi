487f0639453839a27d9d92329deccc65b41ff2bb
Suma151 <sumackaratagi151@gmail.com>
2024-08-26 21:23:15 +0530

    Indentation_Update.c

--- checker.c
+++ checker.c
@@ -1,27 +1,29 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8))
-  {
-    if(temperature < 0 || temperature > 45)
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{
+    if ((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8))
     {
-    printf("Temperature out of range!\n")
+        if (temperature < 0 || temperature > 45)
+        {
+            printf("Temperature out of range!\n")
+        }
+        else if (soc < 20 || soc > 80)
+        {
+            printf("State of Charge out of range!\n");
+        }
+        else
+        {
+            printf("Charge Rate out of range!\n");
+        }
+        return 0;
     }
-    else if(soc < 20 || soc > 80)
-   {
-    printf("State of Charge out of range!\n");
-   } 
-    else
-   {
-    printf("Charge Rate out of range!\n");
-  }
-     return 0;
-  }
-  return 1;
+    return 1;
 }
 
-int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+int main()
+{
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
 }
